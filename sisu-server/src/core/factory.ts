1ea143c81a5dea27fb922743246bf51ed4fe5784
Roman Timashev <roman@tmshv.ru>
2018-10-21 19:31:48 +0300

    server factory update

--- sisu-server/src/core/factory.ts
+++ sisu-server/src/core/factory.ts
@@ -4,7 +4,7 @@ import { IProjectInfo } from "./external";
 export function createProjectInfo(project: IProject): IProjectInfo {
     return {
         name: project.name,
-        id: project._id,
+        id: `${project._id}`,
         uri: project.uri,
         files: project.lastState.projectFilenames,
     };
