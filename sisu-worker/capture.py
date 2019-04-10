7ee95915a4bd5ea9ab740b6d29cb6d272ce2674d
John19840 <49282909+John19840@users.noreply.github.com>
2019-04-10 12:02:38 +0300

    Update capture.py

--- sisu-worker/capture.py
+++ sisu-worker/capture.py
@@ -3,8 +3,7 @@ import scriptcontext as sc
 import Rhino
 import os
 import json
-
-
+# создать скриншот Top/Left/Right/Front окна
 def create_screenshot(view, file):
     rs.ZoomExtents(view, False)
     rs.CurrentView(view)
@@ -12,29 +11,32 @@ def create_screenshot(view, file):
     comm = '-_ViewCaptureToFile {file}'.format(file=file)
     
     rs.Command(comm, False)
-
-
+# функция мэйн
 def main():
     task_file = os.path.expanduser('~/Desktop/sisu_task.json')
     files_dir = os.path.expanduser('~/Desktop/screenshots')
-    file_id = task['fileId']
+    
+    with open(task_file, 'r') as read_file:
+        data = json.load(read_file)
+    
+    file_id = data["fileId"]
     screenshot = os.path.join(files_dir, '{}.png'.format(file_id))
     
     create_screenshot('Top', screenshot)
-    
-    
-# save all named views to jpg via view capture to file
-
-import rhinoscriptsyntax as rs
-
+    capture_named_views()
+# сделать скриншот NamedView, пока что без параметров, когда будет 
+# польностью готов json - можно будет добавить аргументы
 def capture_named_views():
     all_views = rs.NamedViews()
     selected_views= rs.MultiListBox(all_views)
-    filename_base = rs.GetString("base file name","Rics_file_")
+    filename_base = os.path.expanduser('~/Desktop/screenshots/')
     
     for view_name in selected_views:
         rs.RestoreNamedView(view_name)
         rs.Command('-_ViewCaptureToFile '+chr(34)+filename_base+view_name+'.jpg'+chr(34)+' _EnterEnd')
+
+if __name__== "__main__":
+    main()
         
         
 # request
