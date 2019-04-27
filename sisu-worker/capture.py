a52742b1b5fb4fd2dc55e593d8067ca9c5e9863f
Roman Timashev <roman@tmshv.ru>
2019-04-27 23:30:40 +0300

    worker capture update

--- sisu-worker/capture.py
+++ sisu-worker/capture.py
@@ -5,7 +5,11 @@ import os
 import json
 
 
-# создать скриншот Top/Left/Right/Front окна
+def open_file(path):
+    rs.DocumentModified(False)
+    return rs.Command('_-Open "{}" _Enter'.format(path))
+
+
 def create_screenshot(view, file):
     command = '-_ViewCaptureToFile {file}'.format(file=file)
 
@@ -14,13 +18,12 @@ def create_screenshot(view, file):
     rs.Command(command, False)
 
 
-# сделать скриншот NamedView
 def capture_named_views(view, file):
     command = '-_ViewCaptureToFile {file} _EnterEnd'.format(file=file)
-    
+
     all_views = rs.NamedViews()
     selected_views = rs.MultiListBox(all_views)
-    
+
     for view_name in selected_views:
         rs.RestoreNamedView(view_name)
         rs.Command(command)
@@ -28,18 +31,36 @@ def capture_named_views(view, file):
 
 def main():
     task_filename = os.path.expanduser('~/Desktop/sisu_task.json')
-    files_dir = os.path.expanduser('~/Desktop/screenshots')
-    
+
     with open(task_filename, 'r') as f:
         data = json.load(f)
-    
+
     file_id = data['fileId']
-    result_filename = os.path.join(files_dir, '{}.png'.format(file_id))
+    files_dir = data['outputDir']
+    items = data['previews']
 
-    create_screenshot('Top', result_filename)
+    filename = data['filename']
+    s = open_file(filename)
 
-    named_views = rs.NamedViews()
-    capture_named_views(named_views[0], result_filename)
+    # if not s:
+    # task_result = get_task_error('cannot open file {}'.format(filename))
+    # save_task_result(task_result)
+    # return
+
+    i = 0
+    for capture in items:
+        filename = '{file_id}-{index}.png'.format(file_id=file_id, index=i)
+        result_filename = os.path.join(files_dir, filename)
+        viewport = capture['viewport']
+
+        create_screenshot(viewport, result_filename)
+
+        # named_views = rs.NamedViews()
+        # capture_named_views(named_views[0], result_filename)
+        i += 1
+
+    rs.DocumentModified(False)
+    rs.Exit()
 
 
 if __name__ == '__main__':
